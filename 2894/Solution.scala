92ffb32695d899456c19970b1a8454ef498c123d
samjwu <sjwu@ualberta.ca>
2023-10-20 21:30:12 -0600

    2894

---
+++ 2894/Solution.scala
@@ -0,0 +1,16 @@
+object Solution {
+    def differenceOfSums(n: Int, m: Int): Int = {
+        var num1 = 0;
+        var num2 = 0;
+        
+        for (i <- 1 to n) {
+            if (i % m == 0) {
+                num2 += i;
+            } else {
+                num1 += i;
+            }
+        }
+        
+        return num1 - num2;
+    }
+}
