36a91bf2ea117ecb823de40b52e08ad025fd9fa3
samjwu <sjwu@ualberta.ca>
2023-10-21 21:09:29 -0600

    Remove semicolons
    line ending delimiter is idiomatic for Scala

--- 2894/Solution.scala
+++ 2894/Solution.scala
@@ -1,16 +1,16 @@
 object Solution {
     def differenceOfSums(n: Int, m: Int): Int = {
-        var num1 = 0;
-        var num2 = 0;
+        var num1 = 0
+        var num2 = 0
         
         for (i <- 1 to n) {
             if (i % m == 0) {
-                num2 += i;
+                num2 += i
             } else {
-                num1 += i;
+                num1 += i
             }
         }
         
-        return num1 - num2;
+        return num1 - num2
     }
 }
